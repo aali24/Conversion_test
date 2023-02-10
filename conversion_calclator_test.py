720622cf612e4293b19e9c7550910fa83b7d9536
Abdihamid <dahmid@hotmail.com>
2023-02-10 16:39:52 +0000

    Initial Commit

---
+++ conversion_calclator_test.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_unit_conversion():
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://www.calculator.net/conversion-calculator.html")
+
+    # select the length conversion
+    length_conversion = driver.find_element("xpath", '//a[contains(text(), "Length")]')
+    length_conversion.click()
+
+    # input the value to convert
+    input_field = driver.find_element("xpath", '//*[@name="fromVal"]')
+    input_field.send_keys("10")
+
+    # select the unit to convert from
+    unit_from = driver.find_element("xpath", '//select[@name="calFrom"]')
+    unit_from.send_keys("Inches")
+
+    # select the unit to convert to
+    unit_to = driver.find_element("xpath", '//select[@name="calTo"]')
+    unit_to.send_keys("Centimeters")
+
+    # perform the conversion
+    input_field.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    # assert that the conversion was successful
+    result = driver.find_element("xpath", '//input[@name="toVal"]')
+    assert result.get_attribute("value") == "25.4"
+
+    # repeat the above steps for a different conversion
+    input_field = driver.find_element("xpath", '//*[@name="fromVal"]')
+    input_field.clear()
+    input_field.send_keys("20")
+    unit_from = driver.find_element("xpath", '//select[@name="calFrom"]')
+    unit_from.send_keys("Centimeters")
+    unit_to = driver.find_element("xpath", '//select[@name="calTo"]')
+    unit_to.send_keys("Inches")
+    input_field.send_keys(Keys.RETURN)
+    time.sleep(2)
+    result = driver.find_element("xpath", '//input[@name="toVal"]')
+    assert result.get_attribute("value") == "7.874015748"
+
+    # close the browser window
+    driver.quit()
+
+if __name__ == "__main__":
+    test_unit_conversion()
